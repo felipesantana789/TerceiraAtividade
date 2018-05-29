fe0053641ffdabe304d4bbe388c49e2b96749ef9
felipe.ssilva26 <felipe.plbravo@gmail.com>
2018-05-29 17:11:54 -0300

    Initial commit

---
+++ app/src/main/java/br/senac/sp/l13/terceiraatividade/SegundaParteActivity.java
@@ -0,0 +1,13 @@
+package br.senac.sp.l13.terceiraatividade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SegundaParteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segunda_parte_layout);
+    }
+}
